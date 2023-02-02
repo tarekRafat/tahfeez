92317d47be474dd1ce9c86e28aa0814ac2c737d8
Tarek Abdulbary <tarek.abdulbary97@gmail.com>
2023-02-02 08:30:16 +0200

    adding form array to reactive form

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,8 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import { QURAN } from 'src/app/data/Quran';
+import { AbstractControl, FormArray, FormBuilder } from '@angular/forms';
+import { MatSelectChange } from '@angular/material/select';
+import { getBySurahName, QURAN } from 'src/app/data/Quran';
 
-type FormGroupType = {
-  name: string;
-  date: string;
-  surahName: string;
-  start: any[];
-  end: any[];
-  count: any[];
-  note: string;
-};
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -22,40 +14,73 @@ export class HomeComponent {
   startStr: any = { start: '', end: '' };
   endStr: string = '';
   surahName: string = '';
+  formGroup = this.fb.group({
+    name: [''],
+    date: [new Date()],
+    suwar: this.fb.array([this.surahContent()]),
+    revision: this.fb.array([]),
+    note: [''],
+  });
+  get suwar() {
+    return this.formGroup.get('suwar') as FormArray;
+  }
+  get revision() {
+    return this.formGroup.get('revision') as FormArray;
+  }
   constructor(private fb: FormBuilder) {}
 
-  formGroup = this.fb.group<FormGroupType>({
-    name: '',
-    date: '',
-    surahName: '',
-    start: [],
-    end: [],
-    count: [],
-    note: '',
-  });
+  surahContent() {
+    return this.fb.group({
+      name: [''],
+      count: [0],
+      start: [''],
+      end: [''],
+      startStr: [''],
+      endStr: [''],
+    });
+  }
 
   onSubmit() {
     console.log(this.formGroup.value);
   }
-
-  onChangeSurahHandler(e: any) {
+  // onChangeSurahHandler(e: any) {
+  //   console.log(e);
+  // }
+  onChangeSurahHandler(e: any, formGroup: AbstractControl) {
     const value = e.value;
     this.surahName = value;
-    this.formGroup.get('start')?.reset();
-    this.formGroup.get('end')?.reset();
 
-    const filteredValue = this.QURAN.find((qr) => qr.id === value);
+    const filteredValue = this.QURAN.find((qr) => qr.name === value);
 
     const endCountOfEverySurah = Array(
       filteredValue.array[filteredValue.array.length - 1].id
     );
 
-    this.formGroup.get('count')?.patchValue(endCountOfEverySurah);
+    formGroup?.get('count')?.patchValue(endCountOfEverySurah);
   }
-  onChangeFromSelectHandler(felidName: string) {
-    const filteredValue = this.QURAN.find((qr) => qr.id === this.surahName);
-    this.startStr[felidName] =
-      filteredValue.array[this.formGroup.get(felidName)?.value].ar;
+
+  onChangeFromSelectHandler(
+    e: MatSelectChange,
+    propertyName: string,
+    formGroup: AbstractControl
+  ) {
+    console.log(formGroup);
+
+    const surahName = formGroup?.get('name')?.value;
+    const surahNumber = e.value;
+    const surahArr = getBySurahName(surahName);
+    const startStr = surahArr?.array?.find(
+      (item) => item?.id === surahNumber
+    )?.ar;
+    formGroup?.get(propertyName)?.patchValue(startStr);
+  }
+
+  addSurah(revision?: string) {
+    if (revision) {
+      this.revision.push(this.surahContent());
+    } else {
+      this.suwar.push(this.surahContent());
+    }
   }
 }
 // name - date - rate1 -rate2 - dropdown(list) -
